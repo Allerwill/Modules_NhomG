681bb3e958be6c85af57183c288333bdb100f04e
will's home <allerwill.lee@gmail.com>
2019-11-10 15:48:18 +0700

    Duc Fresh base 43-45

---
+++ 44/44-content.php
@@ -0,0 +1,19 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+?>
+<div class="copyright_wrap">
+    <div class="copyright_wrap_inner">
+        <div class="content_wrap">
+            <div class="copyright_text">
+                ThemeREX © 2016 All Rights Reserved
+            </div>
+        </div>
+    </div>
+</div>
